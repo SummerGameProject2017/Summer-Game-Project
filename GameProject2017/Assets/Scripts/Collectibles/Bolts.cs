4b769021d3627bee810cc8b235ea1a9416658607
Bruno Verissimo <brunoverissimo1986@hotmail.com>
2017-05-10 14:49:54 -0400

    Changing Framework. Adding first prefabs for collectables and Enemies

---
+++ GameProject2017/Assets/Scripts/Collectibles/Bolts.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bolts : Collectables
+{
+
+
+    public override void OnStart()
+    {
+    
+    }
+
+    public override void OnUpdate()
+    {
+
+    }
+
+
+}

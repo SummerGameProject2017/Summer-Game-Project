0f248bba1cc7cc59942a8df48d142a2e012560ec
SLCTECH\dsamson <dsamson14@sl.on.ca>
2017-05-11 14:14:50 -0400

    no message

---
+++ GameProject2017/Assets/Scripts/Level Code/Fan.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fan : MonoBehaviour
+{
+
+    public float Force = 5;
+
+    void OnCollisionEnter(Collision col)
+    {
+        Debug.Log("Collision!");
+        if (col.gameObject.name == "Player")
+        {
+            col.gameObject.GetComponent<Rigidbody>().AddForce(0, Force, 0);
+        }
+    }
+}
--- GameProject2017/Assets/Scripts/Level Code/PlayerTester.cs
+++ GameProject2017/Assets/Scripts/Level Code/PlayerTester.cs
@@ -16,7 +16,7 @@ public class PlayerTester : MonoBehaviour
     void FixedUpdate()
     {     
             var x = Input.GetAxis("Horizontal") * Time.deltaTime * 150.0f;
-            var z = Input.GetAxis("Vertical") * Time.deltaTime * 3.0f;
+            var z = Input.GetAxis("Vertical") * Time.deltaTime * speed;
 
             transform.Rotate(0, x, 0);
             transform.Translate(0, 0, z);

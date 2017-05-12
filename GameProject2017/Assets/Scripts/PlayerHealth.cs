92a336fc69ad0db992d93d21bd3326202b27ee98
DESKTOP-LBHIUKJ\Antonio <cantonioq@gmail.com>
2017-05-12 16:20:48 -0400

    updated health ui

--- GameProject2017/Assets/Scripts/PlayerHealth.cs
+++ GameProject2017/Assets/Scripts/PlayerHealth.cs
@@ -4,13 +4,10 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class PlayerHealth : MonoBehaviour {
-    public Image heart;  //reference for hearts
-    public Image heart2;
-    public Image heart3;
-    public Object player;
-
-
-
+    public Image heart;     //reference for hearts
+    public Image heart2;    //heart2
+    public Image heart3;    //heart3    
+    public Object player;   //not currenltly  used//only for testing//can delete
 
     private int energy = 3; //int for energy
     public Text gameOverText;   //reference for text
@@ -28,15 +25,6 @@ public class PlayerHealth : MonoBehaviour {
         if (!isGameOver)
             transform.Translate(Input.GetAxis("Horizontal") * Time.deltaTime * 10f, 0, 0); //get input
         Debug.Log(energy);
-
-    }
-
-
-    IEnumerator Delay()
-    {
-        print(Time.time);
-        yield return new WaitForSecondsRealtime(5);
-        print(Time.time);
     }
 
     //Check if player enters/stays on the fire
@@ -48,16 +36,17 @@ public class PlayerHealth : MonoBehaviour {
             energy -= 1;  //reduce health
             transform.position += new Vector3 (2.0f, 0.0f, 0.0f);
 
-            StartCoroutine(Delay());
-            
             //reverse forward vector
+            //hides the right heart on screen when player get hits once
             heart3.enabled = false;
 
+            //loses second heart when hit a second time
             if (energy == 1)
             {
                 heart2.enabled = false;
             }
 
+            //loses third heart when hit third time
             if (energy == 0)
             {
                 heart.enabled = false;
@@ -66,5 +55,36 @@ public class PlayerHealth : MonoBehaviour {
             }
 
         }
+
+        //for recovering the hearts when picking up cube, oil etc...
+        if (other.gameObject.name == "health test" && energy > 0)
+        {
+            energy += 1;  //reduce health
+
+           // transform.position += new Vector3(2.0f, 0.0f, 0.0f);
+
+            //hides the right heart on screen when player get hits once
+            heart2.enabled = true;
+            Debug.Log("heart2");
+
+            //loses second heart when hit a second time
+            if (energy == 2)
+            {
+                heart3.enabled = true;
+                Debug.Log("heart3");
+            }
+        }
     }
+
+        //this temporarily stops the player after colliding with ennemy(cube)
+        void OnTriggerExit()
+        {
+      /*  
+        for (int i = 0; i < 1000; i++)
+        {
+            Debug.Log("For LOOP");
+        }
+   */
+    }
+   
 }

d3af6edcc765e4970a5e0a9ca138d4beb9121f53
DESKTOP-LBHIUKJ\Antonio <cantonioq@gmail.com>
2017-05-12 14:10:33 -0400

    created the player helath script with the hud for player hearts on screen

---
+++ GameProject2017/Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerHealth : MonoBehaviour {
+    public Image heart;  //reference for hearts
+    public Image heart2;
+    public Image heart3;
+    public Object player;
+
+
+
+
+    private int energy = 3; //int for energy
+    public Text gameOverText;   //reference for text
+    private bool isGameOver = false; //a bool flag to see if game is over
+
+    void Start()
+    {
+        gameOverText.enabled = false; //disables the GameOver text on start
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //check if game is over i.e., health is greater than 0
+        if (!isGameOver)
+            transform.Translate(Input.GetAxis("Horizontal") * Time.deltaTime * 10f, 0, 0); //get input
+        Debug.Log(energy);
+
+    }
+
+
+    IEnumerator Delay()
+    {
+        print(Time.time);
+        yield return new WaitForSecondsRealtime(5);
+        print(Time.time);
+    }
+
+    //Check if player enters/stays on the fire
+    void OnTriggerEnter(Collider other)
+    {
+        //if player triggers fire object and health is greater than 0
+        if (other.gameObject.name == "ennemy test" && energy > 0)
+        {
+            energy -= 1;  //reduce health
+            transform.position += new Vector3 (2.0f, 0.0f, 0.0f);
+
+            StartCoroutine(Delay());
+            
+            //reverse forward vector
+            heart3.enabled = false;
+
+            if (energy == 1)
+            {
+                heart2.enabled = false;
+            }
+
+            if (energy == 0)
+            {
+                heart.enabled = false;
+                isGameOver = true;    //set game over to true
+                gameOverText.enabled = true; //enable GameOver text
+            }
+
+        }
+    }
+}

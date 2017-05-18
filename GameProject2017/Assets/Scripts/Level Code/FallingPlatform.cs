252a0dd93762b04e847edb0dba9e001272856899
SLCTECH\dsamson <dsamson14@sl.on.ca>
2017-05-18 10:13:20 -0400

    Fixed robot collectibles

--- GameProject2017/Assets/Scripts/Level Code/FallingPlatform.cs
+++ GameProject2017/Assets/Scripts/Level Code/FallingPlatform.cs
@@ -56,7 +56,7 @@ public class FallingPlatform : MonoBehaviour
             Instantiate(Platform, Startpos, transform.rotation);
             Destroy(Platform);
             HasFallen = false;
-            TimeFallen = 0;
+            TimeFallen = 0.0f;
         }
 	}
 

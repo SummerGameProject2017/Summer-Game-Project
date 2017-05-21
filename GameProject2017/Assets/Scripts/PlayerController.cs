893b8ddc98f47681fe96e0faaf98b841d4ceed15
Bruno Verissimo <brunoverissimo1986@hotmail.com>
2017-05-21 12:07:47 -0400

    Camera view

--- GameProject2017/Assets/Scripts/PlayerController.cs
+++ GameProject2017/Assets/Scripts/PlayerController.cs
@@ -35,38 +35,26 @@ public class PlayerController : MonoBehaviour
         {
             verticalVelocity = -1;
             jump = 2;
-
         }
 
         else
         {
             verticalVelocity -= gravity * Time.deltaTime;
             //verticalVelocity += Physics.gravity.y * Time.deltaTime;
-            moveVector = lastMove;
+            //moveVector = lastMove;
         }
 
-        //moveVector = Vector3.zero;
-        //if (Input.GetKey(JPGameManager.GM.forward) || Input.GetAxis("Vertical") > 0.5)
-        //{
-        //    moveVector.x = 5;
-        //}
-        //if (Input.GetKey(JPGameManager.GM.backward) || Input.GetAxis("Vertical") < -0.5)
-        //{
-        //    moveVector.x = -5;
-        //}
-        //if (Input.GetKey(JPGameManager.GM.left) || Input.GetAxis("Horizontal") < -0.5)
-        //{
-        //    moveVector.z = 5;
-        //}
-        //if (Input.GetKey(JPGameManager.GM.right) || Input.GetAxis("Horizontal") > 0.5)
-        //{
-        //    moveVector.z = -5;
-        //}
+        moveVector = Vector3.zero;
+        
 
-        moveVector.z = InputManager.GetAxis("Vertical"); //* speed;
-        moveVector.x = InputManager.GetAxis("Horizontal"); //* speed;
 
-       // if ((Input.GetKeyDown(JPGameManager.GM.jump) || Input.GetKeyDown(JPGameManager.GM.joyJump)) && jump >= 1)
+        
+
+        moveVector.z = InputManager.GetAxis("Vertical") * speed; //* speed;
+        moveVector.x = InputManager.GetAxis("Horizontal") * speed; //* speed;
+        moveVector.y = 0;
+
+        // if ((Input.GetKeyDown(JPGameManager.GM.jump) || Input.GetKeyDown(JPGameManager.GM.joyJump)) && jump >= 1)
         if (InputManager.GetButtonDown("Jump") && jump >= 1)
         {
             jump--;
@@ -80,23 +68,44 @@ public class PlayerController : MonoBehaviour
         //    //attack code here;
         //}
 
+        //Rotate player
+        if (moveVector != Vector3.zero)
+        {
+
+            gameObject.transform.rotation = Quaternion.Euler(0.0f, Camera.main.transform.localEulerAngles.y, 0.0f);
+
+            //Quaternion rotation = Quaternion.LookRotation(moveVector);
+            //rotation.x = rotation.z = 0.0f;
+
+            //transform.rotation = rotation;
+        }
 
 
+        gameObject.transform.Translate(moveVector * Time.deltaTime);
 
-        moveVector.y = 0;
+        
         //moveVector.Normalize();
-        moveVector *= speed;
+        //moveVector *= speed;
         moveVector.y = verticalVelocity;
 
-        // Add Aditional movement from level
-        moveVector += additionalmovement;
+        //// Add Aditional movement from level
+        //moveVector += additionalmovement;
 
-        // Clear
-        additionalmovement = Vector3.zero;
+        //// Clear
+        //additionalmovement = Vector3.zero;
 
         controller.Move(moveVector * Time.deltaTime);
-        lastMove = moveVector;
+        //lastMove = moveVector;
 
+
+        //Rotate player
+        //if (moveVector != Vector3.down)
+        //{
+        //    Quaternion rotation = Quaternion.LookRotation(moveVector);
+        //    rotation.x = rotation.z = 0.0f;
+
+        //    transform.rotation = rotation;
+        //}
 
 
     }

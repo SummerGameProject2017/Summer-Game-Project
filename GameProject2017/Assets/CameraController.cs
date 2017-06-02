67dd3cd5af85404f74bff0d88521166c9eeb3b55
DESKTOP-45GRC2M\John <johnperry89@hotmail.com>
2017-06-02 10:06:34 -0400

    camera

---
+++ GameProject2017/Assets/CameraController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+
+    public int offsetX;
+    public int offsetY;
+    public int offsetz;
+    float angle;
+    GameObject player;
+
+    // Use this for initialization
+    void Start() {
+        player = GameObject.FindWithTag("Player");
+        transform.position = new Vector3(player.transform.position.x + offsetX, player.transform.position.y + offsetY, player.transform.position.z + offsetz);
+        transform.LookAt(player.transform.position);
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (InputManager.GetButton("Horizontal") || InputManager.GetButton("Vertical"))
+        {
+            StartCoroutine(MoveCamera());
+        }
+            
+        
+    }
+
+    IEnumerator MoveCamera()
+    {
+        yield return new WaitForSeconds(1);
+        transform.position = Vector3.Lerp(transform.position, new Vector3(player.transform.position.x + offsetX, player.transform.position.y + offsetY, player.transform.position.z + offsetz), Time.deltaTime * 2.0f);
+    }
+}

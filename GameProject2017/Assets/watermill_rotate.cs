dfaaab8d20e9d9bfe81851113b77c4039237f471
DESKTOP-LBHIUKJ\Antonio <cantonioq@gmail.com>
2017-06-05 16:14:56 -0400

    watermill prefab, still under construction

---
+++ GameProject2017/Assets/watermill_rotate.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class watermill_rotate : MonoBehaviour {
+
+    public float speed = 10f;
+    
+    
+    void Update ()
+    {
+        transform.Rotate(Vector3.back, speed * Time.deltaTime);
+    }
+}

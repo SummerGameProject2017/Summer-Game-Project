e64cee3e8198fb1cbdde3117b0db2e61633d8e5b
SLCTECH\dsamson <dsamson14@sl.on.ca>
2017-06-15 15:13:37 -0400

    Updated scene, added rolling tires

---
+++ GameProject2017/Assets/Scripts/Level Code/TireRolling.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TireRolling : MonoBehaviour {
+
+    float Timer = 0.0f;
+    public float RespawnTime;
+    public Transform Tire;
+    Vector3 TireStartPos;
+    Quaternion TireStartRot;
+
+	// Use this for initialization
+	void Start ()
+    {
+        TireStartPos = transform.position;
+        TireStartRot = transform.rotation;
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        Timer += Time.deltaTime;
+
+        if (Timer >= RespawnTime)
+        {
+            Instantiate(Tire, TireStartPos, TireStartRot);
+            Destroy(gameObject);
+            Timer = 0.0f;
+        }
+    }
+}

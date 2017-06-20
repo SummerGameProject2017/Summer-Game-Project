02eeb05f4513359dbbc91f3c2405b8a4ddaecc56
SLCTECH\jperry <johnperry89@hotmail.com>
2017-06-20 11:58:42 -0400

    no message

---
+++ GameProject2017/Assets/Scripts/VRController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.VR;
+
+public class VRController : MonoBehaviour {
+    public bool vive = false;
+	// Use this for initialization
+	void Start () {
+		if (VRDevice.isPresent)
+        {
+            Debug.Log(VRDevice.model);
+            if (VRDevice.model == "Vive MV")
+            {
+                Debug.Log("Vive Connected");
+                    vive = true;
+            }
+            else
+            {
+                Debug.Log("Oculus Connected");
+                vive = false;
+            }
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
